5b777af13b864a5b978ad665c9698502014c88fc
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2020-08-06 17:40:06 -0400

    restructure files and use of global variables

--- www/data/src/webgazerExtractServer.py
+++ www/data/src/webgazerExtractServer.py
@@ -8,10 +8,8 @@ import re
 
 import time
 import datetime
-import pytz
 import math
 
-import base64
 from binascii import a2b_base64
 from decimal import Decimal
 
@@ -25,9 +23,11 @@ import tornado.log
 import tornado.escape
 
 import json, csv, urllib
-import cv2
 import numpy as np
 
+from videoProcessing import readImageRGBA,loadScreenCapVideo,writeScreenCapOutputFrames,openScreenCapOutVideo,\
+    closeScreenCapOutVideo,sendVideoFrame,sendVideoEnd
+import global_variables
 import pdb
 # TODO
 # - Check Aaron's timestamps
@@ -46,24 +46,8 @@ csvDoneName = "gazePredictionsDone.csv"
 
 # Participant characteristics file
 pctFile = "participant_characteristics.csv"
-
-# Options
 writeCSV = True
-onlyWritingVideos = True    # Only process videos where the participant is asked to write into a text field
-writeScreenCapVideo = False
-useAaronCircles = False
 
-# Globals for current state of eye tracking
-tobiiCurrentX = 0
-tobiiCurrentY = 0
-wgCurrentX = 0
-wgCurrentY = 0
-
-
-# Which participant are we on?
-participantDirList = []
-participantPos = -1
-participant = []
 
 
 ##################################################################
@@ -180,7 +164,6 @@ class ParticipantData:
         return "[ParticipantData] Directory: " + self.directory + "  PC or Laptop: " + str(self.pcOrLaptop)
 
     def loadParticipantData(self):
-        global pctFile, writeScreenCapVideo
 
         ########################
         # Load participant characteristics as technical parts
@@ -240,7 +223,7 @@ class ParticipantData:
         # We can remove videos from self.videos here to accomplish this.
         #
         # E.G., if we only wanted video files to do with _writing and the dot tests, then
-        if onlyWritingVideos:
+        if global_variables.onlyWritingVideos:
             self.videos = [x for x in self.videos if (x.filename.find('_writing') >= 0 or x.filename.find('dot_test.') >= 0 or x.filename.find( 'dot_test_final.' ) >= 0)]
         
         ################################
@@ -272,146 +255,15 @@ class ParticipantData:
             self.screencapFile = self.directory + "/" + self.directory + ".flv"
         else:
             print( "Not PC nor Laptop! ERROR ERROR" )
-            writeScreenCapVideo = False
+            global_variables.writeScreenCapVideo = False
             return
 
         # If we're going to write out a screen capture video, then load it
-        if writeScreenCapVideo:
+        if global_variables.writeScreenCapVideo:
             openScreenCapOutVideo( self )
             loadScreenCapVideo( self )
 
-##################################################################
-# Image/video operations
-#
-def readImageRGBA( filename ):
-    # Add an alpha channel, because JavaScript ImageData object requires rgba, and I think it'll be quicker to do it in numpy
-    b_channel, g_channel, r_channel = cv2.split( cv2.imread( filename ) )
-    a_channel = np.ones(b_channel.shape, dtype=b_channel.dtype) * 255
-    return cv2.merge((r_channel, g_channel, b_channel, a_channel))
 
-# p = participant
-def loadScreenCapVideo( p ):
-    global writeScreenCapVideo, useAaronCircles
-
-    ##########################################################################
-    # Load new screen capture video
-    if writeScreenCapVideo:
-        print( "    Loading screen capture video..." )
-        # Check if we're already open; if so, close.
-        if p.screencap != None:
-            p.screencap.release()
-
-        # Open screen capture file
-        p.screencap = cv2.VideoCapture( p.screencapFile )
-        p.screencapFrameRate = p.screencap.get(cv2.CAP_PROP_FPS)
-        p.screencapFrameWidth = p.screencap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        p.screencapFrameHeight = p.screencap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        
-        # JT HACK
-        # For some reason, the 'Laptop' screen recording as twice as large in each dimension as it needs to be
-        if p.pcOrLaptop == "Laptop":
-            p.screencapFrameWidth = p.screencapFrameWidth/2
-            p.screencapFrameHeight = p.screencapFrameHeight/2
-        print( "    Frame details: " + str(p.screencapFrameWidth) + " "  + str(p.screencapFrameHeight) + " " + str(p.screencapFrameRate) + " (" + str(int(p.screencapFrameRate)) + ")" )
-
-        # Overwrite timestamp with Aaron's estimate from circle!
-        # Synchronization video file
-        circleSynchroCSV = p.directory + '/' + p.directory + "_circles.csv"
-
-        if useAaronCircles and os.path.isfile( circleSynchroCSV ):
-            print( "    Using Aaron's circle measurement OCR attempt..." )
-
-            # This file contains _local_ timestamps written in EST
-            circTime = 0
-            circFrame = 0
-            with open( circleSynchroCSV ) as f:
-                cod = csv.reader(f, delimiter=',')
-                for i, row in enumerate(cod):
-                    if i == 1:
-                        circTime = row[2]
-                        circFrame = row[3]
-                        break
-
-            print( "    Circle times + frame: " + str(circTime) + " " + str(circFrame) )
-            # Looks like this: Mon 04:20:14.835098 PM
-            cft = datetime.datetime.strptime(circTime, "%a %I:%M:%S.%f %p")
-            cft = cft.replace( year=ct.year, month=ct.month, day=ct.day )
-            tz = pytz.timezone('US/Eastern')
-            cft = tz.normalize(tz.localize(cft)).astimezone(pytz.utc)
-            print( cft.timestamp() )
-            # Now add on the date of the experiment
-            #ctDay = datetime.datetime( ct.year, ct.month, ct.day )
-            print( "    Subtracting: " + str((float(circFrame) * (1000.0/p.screencapFrameRate))))
-            p.screencapStartTime = (cft.timestamp() * 1000) - int((float(circFrame) * (1000.0/p.screencapFrameRate)))
-
-        print( "    ScreenCapVideo Start Time: " + str(p.screencapStartTime) )
-    #
-    # End screen cap video
-    ##########################################################################
-
-
-# p = participant
-def writeScreenCapOutputFrames( p, frameTimeEpoch ):
-    global tobiiCurrentX, tobiiCurrentY, wgCurrentX, wgCurrentY
-
-    ###########################################################################################################
-    # Display the corresponding video frame
-    msecIntoVid = frameTimeEpoch - p.screencapStartTime
-    
-    # Either we have an initial seek (expensive)
-    if p.prevMSECIntoVideo == -1:
-        # print( "MSEC into video: " + str(msecIntoVid) )
-        p.screencap.set(cv2.CAP_PROP_POS_MSEC, msecIntoVid)
-        ret, image = p.screencap.read()
-        # JHT HACK
-        # For some reason, the 'Laptop' screen recording is twice as large as it needs to be
-        if p.pcOrLaptop == "Laptop":
-            image = cv2.resize( image, (int(p.screencapFrameWidth),int(p.screencapFrameHeight) ) )
-        prevMSECIntoVideo = msecIntoVid
-
-        # Write the frame
-        if ret:
-            center = ( int(p.screencapFrameWidth * float(tobiiCurrentX)), int(p.screencapFrameHeight * float(tobiiCurrentY)) )
-            image = cv2.circle(image, center, 10, (0,255,0), -1)
-            center = ( int(p.screencapFrameWidth * float(wgCurrentX)), int(p.screencapFrameHeight * float(wgCurrentY)) )
-            image = cv2.circle(image, center, 10, (0,0,255), -1)
-            p.screencapOut.write(image)
-    
-    # Or, we just play the video until we hit the right time (cheap)
-    else:
-        # Decode frames until we're at the right place
-        while msecIntoVid > p.screencap.get(cv2.CAP_PROP_POS_MSEC) + int(1000/p.screencap.get(cv2.CAP_PROP_FPS)):
-            print( "    Writing catchup frames..." + str(msecIntoVid) + " " + str(p.screencap.get(cv2.CAP_PROP_POS_MSEC)) )
-            ret, image = p.screencap.read()
-            if p.pcOrLaptop == "Laptop":
-                image = cv2.resize( image, (int(p.screencapFrameWidth),int(p.screencapFrameHeight) ) )
-
-            if ret:
-                center = ( int(p.screencapFrameWidth * float(tobiiCurrentX)), int(p.screencapFrameHeight * float(tobiiCurrentY)) )
-                image = cv2.circle(image, center, 10, (0,255,0), -1)
-                center = ( int(p.screencapFrameWidth * float(wgCurrentX)), int(p.screencapFrameHeight * float(wgCurrentY)) )
-                image = cv2.circle(image, center, 10, (0,0,255), -1)
-                p.screencapOut.write(image)
-
-    # End screen cap video out
-    ###########################################################################################################
-
-
-def openScreenCapOutVideo( p ):
-
-    # Make output video on screen capture
-    print( "    Creating screen capture video output..." )
-    # cv2.VideoWriter_fourcc('H','2','6','4') - if you can build opencv correctly
-    p.screencapOut = cv2.VideoWriter( p.directory + "/" + "screenCapOut_" + p.videos[p.videosPos].filename + ".avi", cv2.VideoWriter_fourcc('M','J','P','G'), int( p.screencapFrameRate ), (int(p.screencapFrameWidth), int(p.screencapFrameHeight)) )
-    p.prevMSECIntoVideo = -1
-
-
-def closeScreenCapOutVideo( p ):
-
-    print( "    Closing screen capture video output..." )
-    # Save out the screen capture output
-    if p.screencapOut != None:
-        p.screencapOut.release()
 
 
 ###########################################################################################################
@@ -440,49 +292,18 @@ def sendParticipantInfo( wsh, participant ):
 
     wsh.write_message( tornado.escape.json_encode( parcel ) )
 
-def sendVideoFrame( wsh, fn, pv ):
-    global tobiiCurrentX, tobiiCurrentY
 
-    # Send the video frame, with the timestamp first
-    # Reminder: "frame_{:08d}_{:08d}.png"
-    frameNum = fn[len(fn)-21:len(fn)-13]
-    timestamp = fn[len(fn)-12:len(fn)-4]
-
-    parcel = ({'msgID': "2",
-               'videoFilename': pv.filename,
-               'frameNum': str(frameNum),
-               'frameNumTotal': str(len(pv.frameFilesList)),
-               'frameTimeEpoch': str(int(timestamp) + pv.startTimestamp),
-               'frameTimeIntoVideoMS': str(timestamp), 
-               'tobiiX': "{:+.4f}".format(tobiiCurrentX),
-               'tobiiY': "{:+.4f}".format(tobiiCurrentY)})
-    wsh.write_message( tornado.escape.json_encode(parcel) )
-    wsh.write_message( readImageRGBA( fn ).tobytes(), binary=True )
-
-def sendVideoEnd( wsh ):
-    global participant
-
-    # Progress video if not at end of video list for participant
-    if participant.videosPos+1 >= len(participant.videos):
-        # New participant!
-        newParticipant( wsh )
-
-    else:
-        # Regular 'video end' message; will trigger return of {'msgID': "1"}
-        parcel = {'msgID': "4"}
-        wsh.write_message( tornado.escape.json_encode(parcel) )
 
 ######################################################################################
 # Processors for messages
 
 # p = participant
 def writeDataToCSV( p, msg ):
-    global tobiiCurrentX, tobiiCurrentY, wgCurrentX, wgCurrentY
 
     ###########################################################################################################
     # Store current WebGazer prediction from browser
-    wgCurrentX = float( msg["webGazerX"] )
-    wgCurrentY = float( msg["webGazerY"] )
+    global_variables.wgCurrentX = float( msg["webGazerX"] )
+    global_variables.wgCurrentY = float( msg["webGazerY"] )
     wgError = float( msg["error"] )
     wgErrorPix = float( msg["errorPix"] )
 
@@ -499,8 +320,8 @@ def writeDataToCSV( p, msg ):
     if p.tobiiListPos == len(p.tobiiList):
         # We've come to the end of the list and there are no more events...
         print( "Error: at end of Tobii event list; no matching timestamp" )
-        tobiiCurrentX = -1
-        tobiiCurrentY = -1
+        global_variables.tobiiCurrentX = -1
+        global_variables.tobiiCurrentY = -1
     else:
         # TobiiList
         diffCurr = frameTimeEpoch - p.tobiiList[p.tobiiListPos].timestamp
@@ -514,20 +335,20 @@ def writeDataToCSV( p, msg ):
 
         # Check validity for return value
         if td.rightEyeValid == 1 and td.leftEyeValid == 1:
-            tobiiCurrentX = (td.leftScreenGazeX + td.rightScreenGazeX) / 2.0
-            tobiiCurrentY = (td.leftScreenGazeY + td.rightScreenGazeY) / 2.0
+            global_variables.tobiiCurrentX = (td.leftScreenGazeX + td.rightScreenGazeX) / 2.0
+            global_variables.tobiiCurrentY = (td.leftScreenGazeY + td.rightScreenGazeY) / 2.0
         elif td.rightEyeValid == 1 and td.leftEyeValid == 0:
-            tobiiCurrentX = td.rightScreenGazeX
-            tobiiCurrentY = td.rightScreenGazeY
+            global_variables.tobiiCurrentX = td.rightScreenGazeX
+            global_variables.tobiiCurrentY = td.rightScreenGazeY
         elif td.rightEyeValid == 0 and td.leftEyeValid == 1:
-            tobiiCurrentX = td.leftScreenGazeX
-            tobiiCurrentY = td.leftScreenGazeY
+            global_variables.tobiiCurrentX = td.leftScreenGazeX
+            global_variables.tobiiCurrentY = td.leftScreenGazeY
         else:
             # Neither is valid, so we could either leave it as the previous case,
             # which involves doing nothing,
             # or set it to -1.
-            tobiiCurrentX = -1
-            tobiiCurrentY = -1
+            global_variables.tobiiCurrentX = -1
+            global_variables.tobiiCurrentY = -1
 
     ###################################################
     # Work out what to write out to CSV
@@ -560,9 +381,11 @@ def writeDataToCSV( p, msg ):
         #                'tobiiLeftScreenGazeX','tobiiLeftScreenGazeY','tobiiRightScreenGazeX','tobiiRightScreenGazeY','webGazerX','webGazerY','clmPos','eyeFeatures'])
 
         # Target dir for output
-        outDir = outputPrefix + '/' + participant.directory + '/' + participant.videos[participant.videosPos].filename + "_frames" + '/'
+        outDir = outputPrefix + '/' + global_variables.participant.directory + '/' + \
+            global_variables.participant.videos[global_variables.participant.videosPos].filename \
+            + "_frames" + '/'
         # Target gaze predictions csv
-        gpCSV = outDir + '/' +  video + csvTempName 
+        gpCSV = outDir + global_variables.participant.videos[global_variables.participant.videosPos].filename + csvTempName 
 
 
         with open( gpCSV, 'a', newline='' ) as f:
@@ -574,36 +397,29 @@ def writeDataToCSV( p, msg ):
     return frameTimeEpoch
 ################################################################################################
 
-def newParticipant( wsh ):
-    global participant, participantPos, participantDirList
-
-    # TODO James doesn't understand python. How do I nuke the 'participant' global?!
-    participant = None
-    
-    participantPos = participantPos + 1
+def newParticipant( wsh ):    
+    global_variables.participantPos = global_variables.participantPos + 1
 
     # Check we're not at the last participant
-    if participantPos >= len(participantDirList):
+    if global_variables.participantPos >= len(global_variables.participantDirList):
         print( "All participants completed." )
         exit()
     else:
         # Load the participant data
-        participant = ParticipantData( participantDirList[participantPos] )
-        participant.loadParticipantData()
-        sendParticipantInfo( wsh, participant )
+        global_variables.participant = ParticipantData( global_variables.participantDirList[global_variables.participantPos] )
+        global_variables.participant.loadParticipantData()
+        sendParticipantInfo( wsh, global_variables.participant )
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
-        global participantPos
 
-        participantPos = -1
+        global_variables.participantPos = -1
         newParticipant( self )
 
  
     def on_message(self, message):
-        global participantDirList, participant, participantPos, onlyWritingVideos
         
         #######################################################################################
         # Video requested from client
@@ -615,9 +431,9 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
             # Extract video frames and find timestamps
             # TODO: Refactor, but be careful. Prickly code
             #
-            participant.videosPos = participant.videosPos + 1
-            pv = participant.videos[participant.videosPos]
-            video = participant.directory + '/' + pv.filename
+            global_variables.participant.videosPos = global_variables.participant.videosPos + 1
+            pv = global_variables.participant.videos[global_variables.participant.videosPos]
+            video = global_variables.participant.directory + '/' + pv.filename
             print( "Processing video: " + video )
 
             #
@@ -730,25 +546,25 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         # 
         elif msg['msgID'] == '3':
             # Parse, manipulate the data and write to CSV
-            frameTimeEpoch = writeDataToCSV( participant, msg )
+            frameTimeEpoch = writeDataToCSV( global_variables.participant, msg )
 
-            if writeScreenCapVideo:
-                writeScreenCapOutputFrames( participant, frameTimeEpoch )
+            if global_variables.writeScreenCapVideo:
+                writeScreenCapOutputFrames( global_variables.participant, frameTimeEpoch )
 
             ##################################
             # Send the next frame of the video
-            pv = participant.videos[participant.videosPos]
+            pv = global_variables.participant.videos[global_variables.participant.videosPos]
             pv.frameFilesPos = pv.frameFilesPos + 1
             
             # If the video frame is the last available video frame, send a message to this effect
             if pv.frameFilesPos >= len(pv.frameFilesList):
 
-                if writeScreenCapVideo:
-                    closeScreenCapOutVideo( participant )
+                if global_variables.writeScreenCapVideo:
+                    closeScreenCapOutVideo( global_variables.participant )
 
-                outDir = outputPrefix + '/' + participant.directory + '/' + pv.filename + "_frames" + '/'
-                gpCSV = outDir + '/' +  video + csvTempName
-                gpCSVDone = outDir + '/' + video + "_" + csvDoneName
+                outDir = outputPrefix + '/' + global_variables.participant.directory + '/' + pv.filename + "_frames" + '/'
+                gpCSV = outDir + global_variables.participant.directory + '/' + pv.filename + csvTempName 
+                gpCSVDone = outDir + global_variables.participant.directory + '/' + pv.filename +  csvDoneName
                 if os.path.isfile( gpCSV ):
                     os.rename( gpCSV, gpCSVDone )
 
@@ -776,22 +592,21 @@ class Application(tornado.web.Application):
 
 
 def main():
-    global participantDirList
+    global_variables.init()
 
     ###########################################################################################################
     # Enumerate all P_ subdirectories if not yet done
     regex = re.compile('P_[0-9][0-9]')
 
-    participantDirList = []
+    global_variables.participantDirList = []
     for root, dirs, files in os.walk('.'):
         for d in dirs:
             if regex.match(d):
-               participantDirList.append(d)
+               global_variables.participantDirList.append(d)
 
-    participantDirList = sorted( participantDirList )
+    global_variables.participantDirList = sorted( global_variables.participantDirList )
 
     # NOTE: This would be the point to filter any participants from the processing
-
 
     ###########################################################################################################
     # Setup webserver
